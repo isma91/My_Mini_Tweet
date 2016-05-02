ab3f89026fb1bb2fb6c6a6117049919b0bc174a1
isma91 <ismaydogmus@gmail.com>
2016-05-02 17:02:49 +0200

    adding a sample of the config file

---
+++ config-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Config-sample.php
+*
+* This is the config file, you need to change the host, dbname, user, password
+* to use this project and make it work !!
+*
+* PHP 7.0.6-1+donate.sury.org~xenial+1 (cli) ( NTS )
+*
+* @category Model
+* @package  Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+return [
+	'host' => 'localhost',
+	'dbname' => 'my_mini_tweet',
+	'user' => 'root',
+	'password' => '******'
+];
