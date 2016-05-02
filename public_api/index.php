4807daa96f9669ad4b18e38afd03d6bfb2afa349
isma91 <ismaydogmus@gmail.com>
2016-05-02 22:34:09 +0200

    changing function name

--- public_api/index.php
+++ public_api/index.php
@@ -6,7 +6,7 @@ $user = new UsersController();
 
 switch ($_POST["action"]) {
 	case 'inscription':
-	echo json_encode(array("error" => "Not a valid action !!", "data" => null));
+	$user->create($_POST["user_lastname"], $_POST["user_firstname"], $_POST["user_email"], $_POST["user_username"], $_POST["user_pass"], $_POST["user_confirm_pass"]);
 	break;
 	default:
 	echo json_encode(array("error" => "Not a valid action !!", "data" => null));
