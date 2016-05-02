22d1c64c0c12c6c3141827e92bfab0a1fe48590a
isma91 <ismaydogmus@gmail.com>
2016-05-02 22:34:21 +0200

    using snake_case

--- controller/UsersController.php
+++ controller/UsersController.php
@@ -5,207 +5,209 @@ use model\Bdd;
 use model\User;
 class UsersController extends User
 {
-	static public function isConnected ()
-	{
-		$bdd = new Bdd();
-		if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
-			return false;
-		}
-		$get = $bdd->getBdd()->prepare('SELECT id, token, login FROM users WHERE id = :id AND token = :token');
-		$get->bindParam(':id', $_SESSION['id']);
-		$get->bindParam(':token', $_SESSION['token']);
-		$get->execute();
+    public function send_json($error, $data)
+    {
+        echo json_encode(array("error" => $error, "data" => $data));
+    }
 
-		$user = $get->fetch(\PDO::FETCH_ASSOC);
-		if ($user) {
-			$_SESSION['login'] = $user['login'];
-			return true;
-		}
-	}
+    static public function is_connected ()
+    {
+        $bdd = new Bdd();
+        if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
+            return false;
+        }
+        $get = $bdd->getBdd()->prepare('SELECT id, token, login FROM users WHERE id = :id AND token = :token');
+        $get->bindParam(':id', $_SESSION['id']);
+        $get->bindParam(':token', $_SESSION['token']);
+        $get->execute();
 
-	public function create ($lastname, $firstname, $login, $email, $pass, $confirm_pass)
-	{
-		$bdd = new Bdd();
+        $user = $get->fetch(\PDO::FETCH_ASSOC);
+        if ($user) {
+            $_SESSION['login'] = $user['login'];
+            return true;
+        }
+    }
 
-		if (strlen($pass) < 5) {
-			$this->setError("Password must be at lest 5 characters !!");
-		}
-		if ($pass !== $confirm_pass) {
-			$this->setError("Password are not the same !!");
-			return false;
-		}
+    public function create ($lastname, $firstname, $email, $login, $pass, $confirm_pass)
+    {
+        $error_create = "";
+        $bdd = new Bdd();
 
-		$pass = $this->_hashPassword($pass);
+        if (strlen($pass) < 5) {
+            $error_create = $error_create . "<p>Password must be at lest 5 characters !!</p>";
+        }
 
-		if (!$this->_updateCheckLogin($login)) {
-			$this->setError('Login already in use');
-			return false;
-		}
-		if (!$this->_updateCheckEmail($email)) {
-			$this->setError('Email already in use');
-			return false;
-		}
+        if ($pass !== $confirm_pass) {
+            $error_create = $error_create . "<p>Password are not the same !!</p>";
+        }
+        if ($error_create === "") {
+            $pass = $this->_hash_password($pass);
+            if (!$this->_check_login($login)) {
+                $this->setError('Login already in use');
+            }
+            if (!$this->_check_email($email)) {
+                $this->setError('Email already in use');
+            }
+            $create = $bdd->getBdd()->prepare('INSERT INTO users (login, firstname, lastname, email, pass, created_at) VALUES (:login, :firstname, :lastname, :email, :pass, NOW())');
+            $create->bindParam(':login', $login);
+            $create->bindParam(':lastname', $lastname);
+            $create->bindParam(':firstname', $firstname);
+            $create->bindParam(':email', $email);
+            $create->bindParam(':pass', $pass);
+            if ($create->execute()) {
+                $this->setError('Congrats ! You can connect');
+            }
+        } else {
+            self::send_json($error_create, null);
+        }
+    }
 
-		$create = $bdd->getBdd()->prepare('INSERT INTO users (login, firstname, lastname, email, pass, created_at) VALUES (:login, :firstname, :lastname, :email, :pass, NOW())');
-		$create->bindParam(':login', $login);
-		$create->bindParam(':lastname', $lastname);
-		$create->bindParam(':firstname', $firstname);
-		$create->bindParam(':email', $email);
-		$create->bindParam(':pass', $pass);
-		if ($create->execute()) {
-			$this->setError('Congrats ! You can connect');
-			return true;
-		}
+    public function update ($login, $email, $lastname, $firstname)
+    {
+        $bdd = new Bdd();
+        
+        if (!$this->_check_login($login)) {
+            $this->setError('Login already in use');
+            return false;
+        }
+        if (!$this->_check_email($email)) {
+            $this->setError('Email already in use');
+            return false;
+        }
 
+        $update = $bdd->getBdd()->prepare('UPDATE users SET login = :login, email = :email, lastname = :lastname, firstname = :firstname, updated_at = NOW() WHERE id = :id AND token = :token AND active = 1');
+        $update->bindParam(':login', $login);
+        $update->bindParam(':email', $email);
+        $update->bindParam(':lastname', $lastname);
+        $update->bindParam(':firstname', $firstname);
+        $update->bindParam(':id', $_SESSION['id']);
+        $update->bindParam(':token', $_SESSION['token']);
+        if ($update->execute()) {
+            header('Location:./');
+            return true;
+        }
+    }
 
-	}
+    public function update_password($new)
+    {
+        $bdd = new Bdd();
 
-	public function update ($login, $email, $lastname, $firstname)
-	{
-		$bdd = new Bdd();
-		
-		if (!$this->_updateCheckLogin($login)) {
-			$this->setError('Login already in use');
-			return false;
-		}
-		if (!$this->_updateCheckEmail($email)) {
-			$this->setError('Email already in use');
-			return false;
-		}
+        $password = $this->_hash_password($new);
 
-		$update = $bdd->getBdd()->prepare('UPDATE users SET login = :login, email = :email, lastname = :lastname, firstname = :firstname, updated_at = NOW() WHERE id = :id AND token = :token AND active = 1');
-		$update->bindParam(':login', $login);
-		$update->bindParam(':email', $email);
-		$update->bindParam(':lastname', $lastname);
-		$update->bindParam(':firstname', $firstname);
-		$update->bindParam(':id', $_SESSION['id']);
-		$update->bindParam(':token', $_SESSION['token']);
-		if ($update->execute()) {
-			header('Location:./');
-			return true;
-		}
-	}
+        $update = $bdd->getBdd()->prepare('UPDATE users SET password = :password WHERE id = :id AND token = :token AND active = 1');
+        $update->bindParam(':password', $password);
+        $update->bindParam(':id', $_SESSION['id']);
+        $update->bindParam(':token', $_SESSION['token']);
+        if ($update->execute()) {
+            return true;
+        }
+        return false;
+    }
 
-	public function updatePassword($new)
-	{
-		$bdd = new Bdd();
+    public function delete ()
+    {
+        $bdd = new Bdd();
 
-		$password = $this->_hashPassword($new);
+        $delete = $bdd->getBdd()->prepare('UPDATE users SET active = 0 WHERE id = :id AND token = :token AND active = 1');
+        $delete->bindParam(':id', $_SESSION['id']);
+        $delete->bindParam(':token', $_SESSION['token']);
+        if ($delete->execute()) {
+            session_destroy();
+            header('Location:./');
+            return true;
+        }
+    }
 
-		$update = $bdd->getBdd()->prepare('UPDATE users SET password = :password WHERE id = :id AND token = :token AND active = 1');
-		$update->bindParam(':password', $password);
-		$update->bindParam(':id', $_SESSION['id']);
-		$update->bindParam(':token', $_SESSION['token']);
-		if ($update->execute()) {
-			return true;
-		}
-		return false;
-	}
+    public function connection ($login, $password)
+    {
+        $bdd = new Bdd();
 
-	public function delete ()
-	{
-		$bdd = new Bdd();
+        $getUser = $bdd->getBdd()->prepare('SELECT id, password FROM users WHERE (login = :login OR email = :login) AND active = 1');
+        $getUser->bindParam(':login', $login);
+        $getUser->execute();
 
-		$delete = $bdd->getBdd()->prepare('UPDATE users SET active = 0 WHERE id = :id AND token = :token AND active = 1');
-		$delete->bindParam(':id', $_SESSION['id']);
-		$delete->bindParam(':token', $_SESSION['token']);
-		if ($delete->execute()) {
-			session_destroy();
-			header('Location:./');
-			return true;
-		}
-	}
+        $user = $getUser->fetch(\PDO::FETCH_ASSOC);
 
-	public function connection ($login, $password)
-	{
-		$bdd = new Bdd();
+        $hash = $user['password'];
+        if (!$hash) {
+            $this->setError("Bad login or password");
+            return false;
+        }
 
-		$getUser = $bdd->getBdd()->prepare('SELECT id, password FROM users WHERE (login = :login OR email = :login) AND active = 1');
-		$getUser->bindParam(':login', $login);
-		$getUser->execute();
+        if (!$this->_check_password($password, $hash)) {
+            $this->setError("Bad login or password");
+            return false;
+        }
 
-		$user = $getUser->fetch(\PDO::FETCH_ASSOC);
+        $this->_update_token($user['id']);
 
-		$hash = $user['password'];
-		if (!$hash) {
-			$this->setError("Bad login or password");
-			return false;
-		}
+        return true;
+    }
 
-		if (!$this->_checkPassword($password, $hash)) {
-			$this->setError("Bad login or password");
-			return false;
-		}
+    private function _update_token($id)
+    {
+        $bdd = new Bdd();
 
-		$this->_updateToken($user['id']);
+        $token = sha1(time() * rand(1, 555));
+        $updateToken = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE id = :id');
+        $updateToken->bindParam(':token', $token, \PDO::PARAM_STR, 60);
+        $updateToken->bindParam(':id', $id);
+        if ($updateToken->execute()) {
+            $_SESSION['token'] = $token;
+            $_SESSION['id'] = $id;
+            return true;
+        }
+    }
 
-		return true;
-	}
+    private function _hash_password($password)
+    {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
 
-	private function _updateToken($id)
-	{
-		$bdd = new Bdd();
+    private function _check_password($password, $hash)
+    {
+        return password_verify($password, $hash);
+    }
 
-		$token = sha1(time() * rand(1, 555));
-		$updateToken = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE id = :id');
-		$updateToken->bindParam(':token', $token, \PDO::PARAM_STR, 60);
-		$updateToken->bindParam(':id', $id);
-		if ($updateToken->execute()) {
-			$_SESSION['token'] = $token;
-			$_SESSION['id'] = $id;
-			return true;
-		}
-	}
+    private function _check_login($login)
+    {
+        $bdd = new Bdd();
 
-	private function _hashPassword($password)
-	{
-		return password_hash($password, PASSWORD_DEFAULT);
-	}
+        if (isset($_SESSION['id'])) {
+            $id = $_SESSION['id'];
+        } else {
+            $id = 0;
+        }
+        $check = $bdd->getBdd()->prepare('SELECT * FROM users WHERE login = :login AND id != :id AND active = 1');
+        $check->bindParam(':login', $login);
+        $check->bindParam(':id', $id);
+        $check->execute();
 
-	private function _checkPassword($password, $hash)
-	{
-		return password_verify($password, $hash);
-	}
+        $user = $check->fetch(\PDO::FETCH_ASSOC);
+        if ($user) {
+            return false;
+        }
+        return true;
+    }
 
-	private function _updateCheckLogin($login)
-	{
-		$bdd = new Bdd();
+    private function _check_email($email)
+    {
+        $bdd = new Bdd();
 
-		if (isset($_SESSION['id'])) {
-			$id = $_SESSION['id'];
-		} else {
-			$id = 0;
-		}
-		$check = $bdd->getBdd()->prepare('SELECT * FROM users WHERE login = :login AND id != :id AND active = 1');
-		$check->bindParam(':login', $login);
-		$check->bindParam(':id', $id);
-		$check->execute();
+        if (isset($_SESSION['id'])) {
+            $id = $_SESSION['id'];
+        } else {
+            $id = 0;
+        }
+        $check = $bdd->getBdd()->prepare('SELECT email FROM users WHERE email = :email AND id != :id AND active = 1');
+        $check->bindParam(':email', $email);
+        $check->bindParam(':id', $id);
+        $check->execute();
 
-		$user = $check->fetch(\PDO::FETCH_ASSOC);
-		if ($user) {
-			return false;
-		}
-		return true;
-	}
+        $user = $check->fetch(\PDO::FETCH_ASSOC);
+        if ($user)
+            return false;
 
-	private function _updateCheckEmail($email)
-	{
-		$bdd = new Bdd();
-
-		if (isset($_SESSION['id'])) {
-			$id = $_SESSION['id'];
-		} else {
-			$id = 0;
-		}
-		$check = $bdd->getBdd()->prepare('SELECT email FROM users WHERE email = :email AND id != :id AND active = 1');
-		$check->bindParam(':email', $email);
-		$check->bindParam(':id', $id);
-		$check->execute();
-
-		$user = $check->fetch(\PDO::FETCH_ASSOC);
-		if ($user)
-			return false;
-
-		return true;
-	}
+        return true;
+    }
 }
