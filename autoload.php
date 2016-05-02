416e6ca9aca05ca8dd651c4154d5715b99208516
isma91 <ismaydogmus@gmail.com>
2016-05-02 16:58:58 +0200

    adding comment

--- autoload.php
+++ autoload.php
@@ -1,4 +1,16 @@
 <?php
+/**
+* Autoload.php
+*
+* No need to use include anymore !!
+*
+* PHP 7.0.6-1+donate.sury.org~xenial+1 (cli) ( NTS )
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
 function autoload($class) {
 	$class = str_replace('\\', '/', $class);
     include $class . '.php';
