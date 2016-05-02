7b9aeebaf355b7b72967dd12672c15f9f03aff9a
isma91 <ismaydogmus@gmail.com>
2016-05-02 17:01:49 +0200

    the home page to sign up/in

---
+++ view/home_page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="My_Mini_Tweet is a good way to test your own ego !!" />
+    <title>Welcome to My_Mini_Tweet</title>
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/mui.min.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/materialize.min.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/google_material_icons.css" />
+    <link media="all" type="text/css" rel="stylesheet" href="media/css/style.css" />
+    <script src="media/js/jquery-2.1.4.min.js"></script>
+    <script src="media/js/mui.min.js"></script>
+    <script src="media/js/materialize.min.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+    <div class="container" id="the_body">
+        <div class="row mui-panel" id="the_menu">
+            <h1 id="title">Welcome to My_Mini_Tweet !!</h1>
+        </div>
+  </div>
+</div>
+<span id="project_path"></span>
+<span id="original_path"></span>
+<span id="current_path"></span>
+</body>
+</html>
