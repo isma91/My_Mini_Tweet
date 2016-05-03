4c2dd6b5f02365ef4f248a6478e3cb26ff3c9457
isma91 <ismaydogmus@gmail.com>
2016-05-03 17:23:41 +0200

    adding more action

--- public_api/index.php
+++ public_api/index.php
@@ -8,6 +8,12 @@ switch ($_POST["action"]) {
 	case 'inscription':
 	$user->create($_POST["user_lastname"], $_POST["user_firstname"], $_POST["user_email"], $_POST["user_username"], $_POST["user_pass"], $_POST["user_confirm_pass"]);
 	break;
+	case 'connexion':
+	$user->connexion($_POST["user_username"], $_POST["user_pass"]);
+	break;
+	case 'get_user_info':
+	$user->get_user_info($_POST["user_id"]);
+	break;
 	default:
 	echo json_encode(array("error" => "Not a valid action !!", "data" => null));
 	break;
