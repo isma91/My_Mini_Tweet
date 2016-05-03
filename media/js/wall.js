7f4e343b816bfbdcc94fc6d957804541d3e3a766
isma91 <ismaydogmus@gmail.com>
2016-05-03 18:48:47 +0200

    logut with ajax request

--- media/js/wall.js
+++ media/js/wall.js
@@ -29,6 +29,25 @@ $(document).ready(function(){
 		}
 	});
 	$(document).on('click', '#div_user_avatar', function() {
+		$('#input_avatar').trigger('click');
 		console.log("display form to add another avatar and list other avatar of the user like an history");
+	});
+	$(document).on('click', '#logout', function() {
+		$.post(path_to_ajax, {action: 'logout', user_id: $('#user_id').text(), token: $(this).attr('token')}, function(data, textStatus) {
+			if (textStatus === "success") {
+				data = JSON.parse(data);
+				console.log(data);
+				if (data.error === null) {
+					Materialize.toast('<p class="alert-success">Logout success !!<p>', 500, 'rounded alert-success');
+					setTimeout(function () {
+						window.location = "?page=home";
+					}, 750);
+				} else {
+					Materialize.toast('<p class="alert-failed">' + data.error + '<p>', 3000, 'rounded alert-failed');
+				}
+			} else {
+				Materialize.toast('<p class="alert-failed">a problem occurred while getting your data in the server !! Please contact the admin of the site !!<p>', 3000, 'rounded alert-failed');
+			}
+		});
 	});
 });
