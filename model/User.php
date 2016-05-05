c667853b19f30d1d3b2f101c3aad350648b2c25c
isma91 <ismaydogmus@gmail.com>
2016-05-05 22:10:23 +0200

    adding commet

--- model/User.php
+++ model/User.php
@@ -1,4 +1,16 @@
 <?php
+/**
+* User.php
+*
+* The empty model of User
+*
+* PHP 7.0.6-1+donate.sury.org~xenial+1 (cli) ( NTS )
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
 namespace model;
 class User
 {
