a3f51cafc1fbfe502ed8c5585477bad560f30460
isma91 <ismaydogmus@gmail.com>
2016-05-06 10:30:33 +0200

    changing tweet getter for pagnation

--- public_api/index.php
+++ public_api/index.php
@@ -56,7 +56,7 @@ if (count($_FILES) !== 0) {
 		$user->send_tweet($_POST["tweet"]);
 		break;
 		case 'get_user_tweet':
-		$user->get_user_tweet($_POST["id"]);
+		$user->get_user_tweet($_POST["id"], $_POST["page"]);
 		break;
 		default:
 		echo json_encode(array("error" => "Not a valid action !!", "data" => null));
